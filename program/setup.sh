3da4662dcd7bb27dff43774340a32ce316368eec
Sourish <70473280+SourishS17@users.noreply.github.com>
2023-08-09 13:34:59 +0100

    Update setup.sh

--- program/setup.sh
+++ program/setup.sh
@@ -54,6 +54,6 @@ sudo cp -f ./config_ap/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.co
 echo "------->4. Enable service"
 
 sudo systemctl enable autohotspot.service
-sudo systemctl enable captiveportal.service
+#sudo systemctl enable captiveportal.service
 
 echo "done setup"
