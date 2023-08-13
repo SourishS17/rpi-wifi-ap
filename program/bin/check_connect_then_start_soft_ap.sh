3f382093ddc6cbf4fb541c665da16c00e7f92844
Sourish <70473280+SourishS17@users.noreply.github.com>
2023-08-13 13:08:29 +0100

    Update check_connect_then_start_soft_ap.sh

--- program/bin/check_connect_then_start_soft_ap.sh
+++ program/bin/check_connect_then_start_soft_ap.sh
@@ -28,9 +28,12 @@ check_wifi_connect_at_pwr_on_then_start_ap(){
 
     if [ $wifi_cnt -gt $max_count ]
     then
-        echo "no wifi at start up. Start AP"
-        # & ==> fork to new process to run AP
-        sudo ./start_ap_fork.sh &
+        if [lsusb | grep -q "OpenThread"]
+        then
+            echo "no wifi at start up. Start AP"
+            # & ==> fork to new process to run AP
+            sudo ./start_ap_fork.sh &
+        fi
     fi
 }
 
